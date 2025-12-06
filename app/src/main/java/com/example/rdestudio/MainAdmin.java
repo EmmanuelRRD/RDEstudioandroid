fe7cd18ea91f65d56488cc89979c36846f0b69e1
EmmanuelRRD <emmanuel.rd@tecjerez.edu.mx>
2025-12-06 09:14:07 -0600

    Clase estudio

--- app/src/main/java/com/example/rdestudio/MainAdmin.java
+++ app/src/main/java/com/example/rdestudio/MainAdmin.java
@@ -1,9 +1,14 @@
 package com.example.rdestudio;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.SearchView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
@@ -22,7 +27,22 @@ public class MainAdmin extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_admin);
 
+        RecyclerView list = findViewById(R.id.lista_recycler);
+        ImageButton agregar = findViewById(R.id.btn_agregar);
+        SearchView buscador = findViewById(R.id.searchView);
 
+        agregar.setOnClickListener(v -> {
+            Dialog dialogo = new Dialog(MainAdmin.this);
+            dialogo.setTitle("Nuevo Cliente");
+            dialogo.setCancelable(true);
+            dialogo.setContentView(R.layout.dialogo);
+            dialogo.show();
+
+            Button cancelar = dialogo.findViewById(R.id.btn_cancelar);
+
+            cancelar.setOnClickListener(v3 -> dialogo.dismiss());
+
+        });
 
     }
 }
--- app/src/main/java/estudio/fotografico/Estudio.java
+++ app/src/main/java/estudio/fotografico/Estudio.java
@@ -1,5 +1,67 @@
 package estudio.fotografico;
 
 public class Estudio {
+    String id;
+    String nombre;
+    String direccion;
+    String terminos;
+    String idEncargado;
+    String idAdministrador;
 
+    public Estudio(String id, String nombre, String direccion, String terminos, String idEncargado, String idAdministrador) {
+        this.id = id;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.terminos = terminos;
+        this.idEncargado = idEncargado;
+        this.idAdministrador = idAdministrador;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTerminos() {
+        return terminos;
+    }
+
+    public void setTerminos(String terminos) {
+        this.terminos = terminos;
+    }
+
+    public String getIdEncargado() {
+        return idEncargado;
+    }
+
+    public void setIdEncargado(String idEncargado) {
+        this.idEncargado = idEncargado;
+    }
+
+    public String getIdAdministrador() {
+        return idAdministrador;
+    }
+
+    public void setIdAdministrador(String idAdministrador) {
+        this.idAdministrador = idAdministrador;
+    }
 }
